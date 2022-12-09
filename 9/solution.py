0804f7808accccb7ddc6099ca095e27ae93c4f17
simehaa <simen.haapnes@gmail.com>
2022-12-09 12:35:16 +0100

    oh, what fun it is to ride

---
+++ 9/solution.py
@@ -0,0 +1,111 @@
+# Read movements
+directions = []
+steps = []
+with open("input.txt", "r") as f:
+    for line in f:
+        d, s = line.split()
+        directions.append(d)
+        steps.append(int(s))
+        
+
+def sign(x):
+    """Handy sign function, return sign of a number (-1, 0, 1)"""
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+
+def move_rope(directions, steps, rope_length, height, width, start_x, start_y):
+    """
+    Move a rope of rope_length number of knots, through a grid of height x width,
+    starting at start_x, start_y.
+
+    Follow a specific movement pattern, given by directions and steps.
+
+    Return the number of grid positions touched by rope's tail.
+    """
+    grid = [[0 for w in range(width)] for h in range(height)]
+
+    # All knots should start at the same position
+    knots = []
+    for n in range(rope_length):
+        knots.append([start_x, start_y])
+
+    for d, s in zip(directions, steps):
+        for step in range(s):
+
+            # Move front knot (head)
+            if d == "U":
+                knots[0][0] -= 1
+            elif d == "D":
+                knots[0][0] += 1
+            elif d == "L":
+                knots[0][1] -= 1
+            elif d == "R":
+                knots[0][1] += 1
+    
+            for i in range(1, len(knots)):
+                # Move following knot (tail)
+
+                # If head is exactly one diagonaly step away, do nothing
+                # H . H
+                # . T .
+                # H . H
+                if abs(knots[i-1][0] - knots[i][0]) == 1 and abs(knots[i-1][1] - knots[i][1]) == 1:
+                    pass
+
+                # If head is exactly one adjacent step away, do nothing
+                # . H .
+                # H T H
+                # . H .
+                elif abs(knots[i-1][0] - knots[i][0]) + abs(knots[i-1][1] - knots[i][1]) == 1:
+                    pass
+
+                # If head is two steps directly up or down, move tail one step towards head
+                # . . H . .
+                # . . . . .
+                # . . T . .
+                # . . . . .
+                # . . H . .
+                elif abs(knots[i-1][0] - knots[i][0]) == 2 and knots[i-1][1] == knots[i][1]:
+                    knots[i][0] += sign(knots[i-1][0] - knots[i][0])
+
+                # If head is two steps directly left or right, move tail one step towards head
+                # . . . . .
+                # . . . . .
+                # H . T . H
+                # . . . . .
+                # . . . . .
+                elif abs(knots[i-1][1] - knots[i][1]) == 2 and knots[i-1][0] == knots[i][0]:
+                    knots[i][1] += sign(knots[i-1][1] - knots[i][1])
+
+                # If the head is a "knight move" away (last outcome, hence 'else'),
+                # move tail diagonally towards head
+                # . H . H .
+                # H . . . H
+                # . . T . .
+                # H . . . H
+                # . H . H .
+                else:
+                    knots[i][0] += sign(knots[i-1][0] - knots[i][0])
+                    knots[i][1] += sign(knots[i-1][1] - knots[i][1])
+
+            # Save tail's touchpoint position in grid
+            grid[knots[-1][0]][knots[-1][1]] = 1
+
+    # Count number of positions touched
+    positions_touched = 0        
+    for h in range(height):
+        positions_touched += sum(grid[h])
+
+    return positions_touched
+
+
+for r in [2, 10]:
+    print(
+        "Number of positions touched by tail: "
+        f"{move_rope(directions, steps, r, 500, 500, 250, 250)}, rope length: {r} knots"
+    )
