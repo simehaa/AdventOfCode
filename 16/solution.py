95b7e31cd147579c4b88fb4c59731de6f5902c8d
simehaa <simen.haapnes@gmail.com>
2023-12-05 13:17:05 +0100

    part 1 done

--- 16/solution.py
+++ 16/solution.py
@@ -1,3 +1,5 @@
+import time
+
 # Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 solution = [
     "startat AA",
@@ -27,9 +29,9 @@ solution = [
     "open CC",
 ]
 
-def read_file():
+def read_file(filename):
     valves = {}
-    with open("input.txt", "r") as f:
+    with open(filename, "r") as f:
         for line in f:
             words = line.split(" ")
             valves[words[1]] = {
@@ -39,27 +41,74 @@ def read_file():
     return valves
 
 
-if __name__ == "__main__":
-    valves = read_file()
-    
-    max_minutes = 30
-    max_tunnel_visits = max_minutes // 2
+def is_loop(path):
+    tunnels_visited = []
+    for step in path[::-1]:
+        a, t = step.split(" ")
+        if a in ["moveto", "startat"]:
+            if t in tunnels_visited:
+                return True
+            else:
+                tunnels_visited.append(t)
+        else:
+            return False
+    return False
+
+
+def pressure(path, valves):
+    pressure_released = 0
+    valves_open = []
+    for minute in range(1, 31): # 1, 2, ..., 30
+        for valve in valves_open:
+            pressure_released += valves[valve]["rate"]
+        
+        if minute < len(path):
+            action, tunnel = path[minute].split(" ")
+            if action == "open" and tunnel not in valves_open:
+                valves_open.append(tunnel)
+    return pressure_released
+
+
+def valves_opened(path):
+    valves_open = []
+    for step in path:
+        _action, _tunnel = step.split(" ")
+        if _action == "open" and _tunnel not in valves_open:
+            valves_open.append(_tunnel)
+    return valves_open
+
+
+def visited_opened_and_current(path):
+    valves_visited = []
+    valves_open = []
+    for step in path[:-1]:
+        _action, _tunnel = step.split(" ")
+        if _tunnel not in valves_visited:
+            valves_visited.append(_tunnel)
+        if _action == "open" and _tunnel not in valves_open:
+            valves_open.append(_tunnel)
+        
+    a, t = path[-1].split(" ")
+
+    string = "".join(sorted(valves_visited)) + "_" + "".join(sorted(valves_open)) + "_" + t
+    return string
+
+
+def walk_and_open_valves(filename):
+    valves = read_file(filename)
     tunnel = "AA"
     valves.keys()
     unfinished_paths = [["startat AA"]]
     finished_paths = []
-    done = False
-    
+    done = False    
+    first_valve_opened_at = {} 
     valves_with_flow = []
     for valve in valves:
         if valves[valve]["rate"] > 0:
             valves_with_flow.append(valve)
 
-    first_valve_opened_at = {}    
-
-    for i in range(29):
+    for i in range(30):
         print(f"{i+1}/30, paths: {len(unfinished_paths)}")
-    
         new_paths = []
 
         for path in unfinished_paths:
@@ -67,27 +116,11 @@ if __name__ == "__main__":
             next_tunnels = valves[tunnel]["to"].copy()
 
             # Avoid walking in loops
-            tunnels_visited = []
-            loop = False
-            for step in path[::-1]:
-                _action, _tunnel = step.split(" ")
-                if _action == "open":
-                    break
-                else:
-                    if _tunnel in tunnels_visited:
-                        loop = True
-                        break
-                    else:
-                        tunnels_visited.append(_tunnel)
-            if loop:
+            if is_loop(path):
                 continue
 
             # Check how many valves have been opened in this path
-            valves_opened = []
-            for step in path:
-                _action, _tunnel = step.split(" ")
-                if _action == "open" and _tunnel not in valves_opened:
-                    valves_opened.append(_tunnel)
+            valves_open = valves_opened(path)
 
             if len(next_tunnels) > 1:
                 try:
@@ -111,17 +144,17 @@ if __name__ == "__main__":
                 new_paths.append(new_path)
 
             # Check if it is worth to open valve (not opened before and has flow)
-            if tunnel not in valves_opened and valves[tunnel]["rate"] > 0:
+            if tunnel not in valves_open and valves[tunnel]["rate"] > 0:
                 new_path = path + [f"open {tunnel}"]
 
-                if len(valves_opened) == 0:
+                if len(valves_open) == 0:
                     if tunnel not in first_valve_opened_at:
                         first_valve_opened_at[tunnel] = {"path": new_path}
                     elif len(new_path) >= len(first_valve_opened_at[tunnel]["path"]):
                         continue
 
                 # Is this the last valve?
-                if len(valves_opened) + 1 == len(valves_with_flow):
+                if len(valves_open) + 1 == len(valves_with_flow):
                     finished_paths.append(new_path)
                     continue
                 
@@ -129,34 +162,32 @@ if __name__ == "__main__":
                 else:
                     new_paths.append(new_path)
 
+        # Only keep the best path from each last position
+        unfinished_paths = []
+        paths_at_valve = {}
+        puv = ""
+        for path in new_paths:
             
+            if not is_loop(path):
+                uv = visited_opened_and_current(path)
+                
+                if uv not in paths_at_valve:
+                    paths_at_valve[uv] = path
+                
+                # Check which is best
+                elif pressure(path, valves) > pressure(paths_at_valve[uv], valves):
+                    paths_at_valve[uv] = path
 
-        unfinished_paths = new_paths
-    finished_paths += unfinished_paths
-
-    # Check how much pressure is released for each path
-    most_pressure_released = 0
-    for path in finished_paths:
-        valves_opened = []
-        pressure_released = 0
-
-        for minute in range(1, max_minutes+1): # 1, 2, ..., 30
-            for valve in valves_opened:
-                pressure_released += valves[valve]["rate"]
-            # print("minute", minute + 1, "pressure released:", pressure_released, end=" ")
-            
-            if minute < len(path):
-                action, tunnel = path[minute].split(" ")
-                if action == "open" and tunnel not in valves_opened:
-                    valves_opened.append(tunnel)
-                    # print("opened", tunnel, end=" ")
-                elif action == "moveto":
-                    pass
-                    # print("move to", tunnel, end=" ")
-            # print()
+        for vo, path in paths_at_valve.items():
+            unfinished_paths.append(path)
     
-        most_pressure_released = max(most_pressure_released, pressure_released)
+    most_pressure_released = 0
+    for path in finished_paths + unfinished_paths:
+        most_pressure_released = max(most_pressure_released, pressure(path, valves))
 
-    print("most pressure released:", most_pressure_released)
+    return most_pressure_released
 
-        
+
+if __name__ == "__main__":
+    # print(walk_and_open_valves("test.txt")) # 1651
+    # print(walk_and_open_valves("input.txt")) # 1659
