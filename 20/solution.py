4b8f6dcdc9cc6ed0c0905ccd71d0cfec78d8c933
simehaa <simen.haapnes@gmail.com>
2023-12-05 13:17:06 +0100

    Finally got it: it helps not manipulating the target object I'm trying to keep constant...

--- 20/solution.py
+++ 20/solution.py
@@ -3,24 +3,18 @@ def read_input(filename):
         return [eval(i) for i in f.readlines()]
         
         
-def decrypt(encrypted_list):
+def decrypt(encrypted_list, repeat=1):
     n = len(encrypted_list)
     idx_tracker = list(range(n)) # [0, 1, 2, 3, 4, 5]
-    for i in idx_tracker:
+    for i in idx_tracker.copy() * repeat:
         idx = idx_tracker.index(i)
-        elem = idx_tracker.pop(idx)
-        pos = (encrypted_list[i] + idx) % len(idx_tracker)
-        idx_tracker.insert(pos, i)
-        encrypted_list = [encrypted_list[j] for j in idx_tracker]
-        print(f"Popped {elem} from index {idx}, and re-inserted at index {pos}")
-        print("after", [encrypted_list[j] for j in idx_tracker])
-        print()
-        # print([encrypted_list[idx] for idx in idx_tracker], "\n")
+        idx_tracker.pop(idx)
+        idx_tracker.insert((idx + encrypted_list[i]) % len(idx_tracker), i)
 
-    # zero = idx_tracker.index(encrypted_list.index(0))
+    return [encrypted_list[idx] for idx in idx_tracker]
+    
 
-
-def extract_groove_coordinated(decrypted_list, indices=[1000, 2000, 3000], start_at=0):
+def extract_groove_coordinates(decrypted_list, indices=[1000, 2000, 3000], start_at=0):
     offset = decrypted_list.index(start_at)
     N = len(decrypted_list)
     coordinates = [decrypted_list[(i + offset) % N] for i in indices]
@@ -28,6 +22,14 @@ def extract_groove_coordinated(decrypted_list, indices=[1000, 2000, 3000], start
        
 
 if __name__ == "__main__":  
-    encrypted_list = read_input("test.txt")
-    decrypt(encrypted_list)
-    #print(extract_groove_coordinated([1, 2, -3, 4, 0, 3, -2]))
+    encrypted_list = read_input("input.txt")
+    decrypted_list = decrypt(encrypted_list)
+    coordinates = extract_groove_coordinates(decrypted_list)
+    
+    print(f"Part 1: {sum(coordinates)}")
+
+    key = 811589153
+    decrypted_list = decrypt([i*key for i in encrypted_list], repeat=10)
+    coordinates = extract_groove_coordinates(decrypted_list)
+    
+    print(f"Part 2: {sum(coordinates)}")
