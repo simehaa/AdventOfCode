21c2e33dbb30e0237d94487a25814377c295d114
simehaa <simen.haapnes@gmail.com>
2023-12-05 13:17:11 +0100

    brush up day 1

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -1,29 +1,24 @@
-def day1():
-    table = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-    }
-
+def solve(part, test=False):
+    filename = f"part{part}.txt" if test else "input.txt"
+    written_digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     calibration_sum = 0
-    with open("input.txt", "r") as f:
+    with open(filename, "r") as f:
         lines = f.readlines()
         for line in lines:
             words = line.split()[0]
             digits = []
-            for j, c in enumerate(words):
+            for idx, value in enumerate(words):
                 try:
-                    digits.append(int(c))
+                    digits.append(int(value))
                 except:
-                    for k, v in table.items():
-                        if words[j:].startswith(k):
-                            digits.append(v)
+                    if part == 2:
+                        for digit, written_digit in enumerate(written_digits):
+                            if words[idx:].startswith(written_digit):
+                                digits.append(digit)
             calibration_sum += int(f"{digits[0]}{digits[-1]}")
-    print(calibration_sum)
+    return calibration_sum
+
+print(f"Part 1 (test): {solve(1, test=True)}")
+print(f"Part 2 (test): {solve(2, test=True)}")
+print(f"Part 1: {solve(1)}")
+print(f"Part 2: {solve(2)}")
