3ce24533346761ee15bf725593862b2bd6f54684
Simen Håpnes <simen.haapnes@gmail.com>
2023-12-06 21:45:20 +0100

    fixed solution to hold for all edge cases

--- 2023/6/solution.py
+++ 2023/6/solution.py
@@ -1,10 +1,32 @@
-from math import sqrt, ceil, prod
+from math import ceil, floor, prod, sqrt
 
-with open("input.txt", "r") as f:
-    lines = f.readlines()
-    _, race_times = lines[0].split(":")
-    _, record_distances = lines[1].split(":")
 
-ways = lambda t, d: ceil(sqrt(t**2 - 4*d))
-print("Part 1:", prod([ways(int(t), int(d)) for t, d in zip(race_times.split(), record_distances.split())]))
-print("Part 2:", ways(int(race_times.replace(" ", "")), int(record_distances.replace(" ", ""))))
+def read_input(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        _, race_times = lines[0].split(":")
+        _, record_distances = lines[1].split(":")
+    return race_times, record_distances
+
+
+def ways_to_win(race_time, record_distance):
+    sq = sqrt(race_time**2 - 4*record_distance)
+    if sq.is_integer():
+        if ((race_time - sq)/2).is_integer():
+            return floor(sq) - 1
+        else:
+            return floor(sq)
+    else:
+        low = floor((race_time - sq)/2)
+        high = ceil((race_time + sq)/2)
+        return high - low - 1
+
+
+if __name__ == "__main__":
+    for filename, solutions in zip(["test.txt", "input.txt"], [(288, 71503), (503424, 32607562)]):
+        print(filename)
+        race_times, record_distances = read_input(filename)
+        part_1 = prod([ways_to_win(int(t), int(d)) for t, d in zip(race_times.split(), record_distances.split())])
+        part_2 = ways_to_win(int(race_times.replace(" ", "")), int(record_distances.replace(" ", "")))
+        print("Part 1:", part_1, part_1 == solutions[0])
+        print("Part 2:", part_2, part_2 == solutions[1])
