318cbb4b0010cbb28fef240c19f9738bb63f5d5b
Simen Håpnes <simen.haapnes@gmail.com>
2023-12-13 07:14:57 +0100

    part 2 not quite done

--- 2023/13/solution.py
+++ 2023/13/solution.py
@@ -23,13 +23,12 @@ def find_row(mirror, smudge=False):
             if mirror[i-j-1] != mirror[i+j]:
                 reflection = False
                 break
-
         if reflection:
             return i
     return False
 
 
-def find_column(mirror, smudge=False):
+def find_column(mirror):
     height = len(mirror)
     width = len(mirror[0])
     for i in range(1, width):
@@ -41,8 +40,54 @@ def find_column(mirror, smudge=False):
             if left != right:
                 reflection = False
                 break
-
         if reflection:
+            return i
+    return False
+
+
+def find_row_with_smudge(mirror):
+    height = len(mirror)
+    for i in range(1, height):
+        length = min(i, height-i)
+        number_of_smudges_needed = 0
+        for j in range(length):
+            if mirror[i-j-1] == mirror[i+j]:
+                continue
+            elif number_of_smudges_needed == 0:
+                number_of_unequal_characters = 0
+                for left, right in zip(mirror[i-j-1], mirror[i+j]):
+                    if left != right:
+                        number_of_unequal_characters += 1
+                if number_of_unequal_characters == 1:
+                    number_of_smudges_needed += 1
+                    continue
+            break
+        if number_of_smudges_needed == 1:
+            return i
+    return False
+
+
+def find_column_with_smudge(mirror):
+    height = len(mirror)
+    width = len(mirror[0])
+    for i in range(1, width):
+        length = min(i, width-i)
+        number_of_smudges_needed = 0
+        for j in range(length):
+            left = [mirror[x][i-j-1] for x in range(height)]
+            right = [mirror[x][i+j] for x in range(height)]
+            if left == right:
+                continue
+            elif number_of_smudges_needed == 0:
+                number_of_unequal_characters = 0
+                for l, r in zip(left, right):
+                    if l != r:
+                        number_of_unequal_characters += 1
+                if number_of_unequal_characters == 1:
+                    number_of_smudges_needed += 1
+                    continue
+            break
+        if number_of_smudges_needed == 1:
             return i
     return False
 
@@ -50,11 +95,25 @@ def find_column(mirror, smudge=False):
 if __name__ == "__main__":
     mirrors = readfile("input.txt")
     total_sum = 0
-    for mirror in mirrors:
+    for i, mirror in enumerate(mirrors):
         row = find_row(mirror)
+        column = find_column(mirror)
         if isinstance(row, int):
             total_sum += 100*row
-        column = find_column(mirror)
-        if isinstance(column, int):
+        elif isinstance(column, int):
             total_sum += column
-    print(total_sum)
+    print("Part 1:", total_sum)
+
+    total_sums = []
+    for i, mirror in enumerate(mirrors):
+        row = find_row_with_smudge(mirror)
+        column = find_column_with_smudge(mirror)
+        if isinstance(row, int) and isinstance(column, int):
+            print(i, "Both row and column is int")
+        elif isinstance(row, int):
+            total_sum += 100*row
+        elif isinstance(column, int):
+            total_sum += column
+    print("Part 2:", total_sum)
+
+# 34382 too low
