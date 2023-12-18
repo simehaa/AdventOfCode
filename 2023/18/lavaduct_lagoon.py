910103dc95fbc321e7e67e7dfb00d181fa5cd9d9
simehaa <simen.haapnes@gmail.com>
2023-12-18 10:39:15 +0100

    speedup part1 using shoelace formula

--- 2023/18/lavaduct_lagoon.py
+++ 2023/18/lavaduct_lagoon.py
@@ -1,68 +1,13 @@
-import random, re
-
-instructions = []
+area = 0
+y, x = 0, 0
 for l in open("input.txt").readlines():
     d, s, c = l.split()
-    instructions.append({
-        "direction": [(0, 1), (1, 0), (0, -1), (-1, 0)][["R", "D", "L", "U"].index(d)],
-        "steps": int(s),
-        "color": c[1:-1],
-    })
+    dy, dx = [(0, 1), (1, 0), (0, -1), (-1, 0)][["R", "D", "L", "U"].index(d)]
+    steps = int(s)
+    color = c[1:-1]
+    ny = y + steps*dy
+    nx = x + steps*dx
+    area += (y + ny)*(x - nx) + steps
+    y, x = ny, nx
 
-
-# dig trench
-y, x = 0, 0
-dug_out = [(y, x)]
-for ins in instructions:
-    dy, dx = ins["direction"]
-    for s in range(ins["steps"]):
-        y += dy
-        x += dx
-        if (y, x) not in dug_out:
-            dug_out.append((y, x))
-
-min_y = 1e10
-min_x = 1e10
-max_y = -1e10
-max_x = -1e10
-for y, x in dug_out:
-    min_y = min(min_y, y)
-    min_x = min(min_x, x)
-    max_y = max(max_y, y)
-    max_x = max(max_x, x)
-
-# find one coordinate on the inside
-while True:
-    y = random.randint(min_y+1, max_y)
-    x = random.randint(min_x+1, max_x)
-    if (y, x) in dug_out:
-        continue
-    row = ""
-    for i in range(x, max_x+1):
-        if (y, i) in dug_out:
-            row += "#"
-        else:
-            row += "."
-    matches = re.findall(r"\#+", row)
-    if len(matches) % 2 == 1:
-        inside = (y, x)
-        break
-
-# flood fill from inside
-inner_coordinates = [inside]
-for y, x in inner_coordinates:
-    for ny, nx in [(y+1, x), (y, x+1), (y-1, x), (y, x-1)]:
-        if (ny, nx) not in dug_out and (ny, nx) not in inner_coordinates:
-            inner_coordinates.append((ny, nx))
-
-# for y in range(min_y, max_y+1):
-#     for x in range(min_x, max_x+1):
-#         if (y, x) in inner_coordinates:
-#             print("O", end="")
-#         elif (y, x) in dug_out:
-#             print("#", end="")
-#         else:
-#             print(".", end="")
-#     print()
-    
-print(len(dug_out) + len(inner_coordinates))
+print(int(area/2) + 1)
