c8c3148456c57fe63d6a1d8584129241148486fd
simehaa <simen.haapnes@gmail.com>
2023-12-18 11:01:13 +0100

    Day 18 complete, fast using shoelace

--- 2023/18/lavaduct_lagoon.py
+++ 2023/18/lavaduct_lagoon.py
@@ -1,13 +1,21 @@
-area = 0
-y, x = 0, 0
-for l in open("input.txt").readlines():
-    d, s, c = l.split()
-    dy, dx = [(0, 1), (1, 0), (0, -1), (-1, 0)][["R", "D", "L", "U"].index(d)]
-    steps = int(s)
-    color = c[1:-1]
-    ny = y + steps*dy
-    nx = x + steps*dx
-    area += (y + ny)*(x - nx) + steps
-    y, x = ny, nx
+def area_of_trench(filename, part=1):
+    area = 0
+    y, x = 0, 0
+    for l in open(filename).readlines():
+        d, s, c = l.split()
+        if part == 1:
+            dy, dx = [(0, 1), (1, 0), (0, -1), (-1, 0)][["R", "D", "L", "U"].index(d)]
+            steps = int(s)
+        elif part == 2:
+            steps = int(c[2:-2], 16)
+            d = int(c[-2], 16)
+            dy, dx = [(0, 1), (1, 0), (0, -1), (-1, 0)][d]
+        ny = y + steps*dy
+        nx = x + steps*dx
+        area += (y + ny)*(x - nx) + steps
+        y, x = ny, nx
+    return int(area/2) + 1
 
-print(int(area/2) + 1)
+
+print("Part 1:", area_of_trench("test.txt", part=1))
+print("Part 2:", area_of_trench("test.txt", part=2))
