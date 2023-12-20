bb2713521cff478999aee1bf11133b99d1f5f188
simehaa <simen.haapnes@gmail.com>
2023-12-20 10:57:46 +0100

    refactor for prettyness

--- 2023/19/aplenty.py
+++ 2023/19/aplenty.py
@@ -1,5 +1,4 @@
 import re
-from operator import lt, gt
 
 
 workflows = {}
@@ -15,34 +14,31 @@ for line in ratings_lines.splitlines():
 
 def execute_workflow(x, m, a, s, name="in"):
     for condition in workflows[name]:
-        if ":" in condition:
-            test, next_name = condition.split(":")
-            if eval(test):
-                break
-        else:
-            next_name = condition
+        if ":" not in condition:
             break
-    if next_name == "A":
+        test, condition = condition.split(":")
+        if eval(test):
+            break
+    if condition == "A":
         return True
-    elif next_name == "R":
+    elif condition == "R":
         return False
     else:
-        return execute_workflow(x, m, a, s, next_name)
-
-
-def permutations(ratings):
-    perm = 1
-    for l in "xmas":
-        perm *= len(ratings[l])
-    return perm
+        return execute_workflow(x, m, a, s, condition)
 
 
 def get_combinations(ratings, name="in", step=0):
+    if name == "A":
+        return len(ratings["x"])*len(ratings["m"])*len(ratings["a"])*len(ratings["s"])
+    elif name == "R":
+        return 0
     condition = workflows[name][step]
-    if ":" in condition:
-        test, pass_name = condition.split(":")
+    if ":" not in condition:
+        return get_combinations(ratings, name=condition, step=0)
+    else:
+        test, next_name = condition.split(":")
         letter = test[0]
-        operator = lt if test[1] == "<" else gt
+        operator = int.__lt__ if test[1] == "<" else int.__gt__
         comparison_value = int(test[2:])
         passed_ratings = {letter: []}
         failed_ratings = {letter: []}
@@ -55,20 +51,7 @@ def get_combinations(ratings, name="in", step=0):
                 passed_ratings[letter].append(our_value)
             else:
                 failed_ratings[letter].append(our_value)
-        if pass_name == "A":
-            accepted_combinations = permutations(passed_ratings)
-        elif pass_name == "R":
-            accepted_combinations = 0
-        else:
-            accepted_combinations = get_combinations(passed_ratings, name=pass_name, step=0)
-        return accepted_combinations + get_combinations(failed_ratings, name=name, step=step+1)
-    else:
-        if condition == "A":
-            return permutations(ratings)
-        elif condition == "R":
-            return 0
-        else:
-            return get_combinations(ratings, name=condition, step=0)
+        return get_combinations(passed_ratings, name=next_name, step=0) + get_combinations(failed_ratings, name=name, step=step+1)
         
 
 print("Part 1:", sum([sum(r) if execute_workflow(*r) else 0 for r in ratings]))
